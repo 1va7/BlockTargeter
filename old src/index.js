804bd18b5a69fd7235ea2f2353f56474fe4f353b
va7 <yehuaiqian@uchicago.edu>
2024-02-19 15:02:54 +0800

    0219

--- old src/index.js
+++ old src/index.js
@@ -52,41 +52,9 @@ function fetchApiStats(apiUrl, apiKey, requester, target) {
         "User-Agent": "phat-contract",
         "Authorization": `${apiKey}`
     };
-    // for wallet balance
-    const getUSDTBalance = JSON.stringify({ query: `
-        query GetUsdtBalance {
-      Ethereum: TokenBalances(
-        input: {filter: {owner: {_eq: "${target}"}, tokenAddress: {_eq: "0xdAC17F958D2ee523a2206206994597C13D831ec7"}}, blockchain: ethereum}
-      ) {
-        TokenBalance {
-          formattedAmount
-        }
-      }
-    }
-    ` });
-    const getUSDCBalance = JSON.stringify({ query: `
-        query GetUsdcBalance {
-      Ethereum: TokenBalances(
-        input: {filter: {owner: {_eq: "${target}"}, tokenAddress: {_eq: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"}}, blockchain: ethereum}
-      ) {
-        TokenBalance {
-          formattedAmount
-        }
-      }
-    }
-    ` });
-    const getWETHBalance = JSON.stringify({ query: `
-          query GetWethBalance {
-      Ethereum: TokenBalances(
-        input: {filter: {owner: {_eq: "${target}"}, tokenAddress: {_eq: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"}}, blockchain: ethereum}
-      ) {
-        TokenBalance {
-          formattedAmount
-        }
-      }
-    }
-    ` });
-    //for socials
+    const sentTokensToTarget = JSON.stringify({ query: `
+  query GetTokenTransfers { ethereum: TokenTransfers(input: {filter: {from: {_in: ["${requester}"]}, to: {_eq: "${target}"}}, blockchain: ethereum}) { TokenTransfer { from { addresses domains { name } socials { dappName profileName profileTokenId profileTokenIdHex userId userAssociatedAddresses } } to { addresses domains { name } socials { dappName profileName profileTokenId profileTokenIdHex userId userAssociatedAddresses } } transactionHash } } polygon: TokenTransfers(input: {filter: {from: {_in: ["${requester}"]}, to: {_eq: "${target}"}}, blockchain: polygon}) { TokenTransfer { from { addresses domains { name } socials { dappName profileName profileTokenId profileTokenIdHex userId userAssociatedAddresses } } to { addresses domains { name } socials { dappName profileName profileTokenId profileTokenIdHex userId userAssociatedAddresses } } transactionHash } } }
+  ` });
     const hasLensProfile = JSON.stringify({ query: `
   query GetLensProfile { Socials( input: { filter: { dappName: { _eq: lens } identity: { _in: ["${target}"] } } blockchain: ethereum } ) { Social { profileName profileTokenId profileTokenIdHex }}}
   ` });
@@ -96,45 +64,19 @@ function fetchApiStats(apiUrl, apiKey, requester, target) {
     const hasPrimaryEns = JSON.stringify({ query: `
   query GetPrimaryEns { Domains(input: {filter: {owner: {_in: ["${target}"]}, isPrimary: {_eq: true}}, blockchain: ethereum}) { Domain { name owner isPrimary }}}
   ` });
-    // for nfts
-    const hasNFTKinds = JSON.stringify({ query: `
-      query GetNFTKinds {
-      Ethereum: TokenBalances(
-        input: {filter: {owner: {_eq: "${target}"}}, blockchain: ethereum}
-      ) {
-        TokenBalance {
-          tokenNfts {
-            id
-          }
-        }
-      }
-    }
-    ` });
-    // for poaps
-    const hasPoapKinds = JSON.stringify({ query: `
-        query GetPoapKinds {
-      Wallet(
-        input: {identity: "${target}", blockchain: ethereum}
-      ) {
-        poaps {
-          id
-        }
-      }
-    }
-    ` });
+    const hasCommonPoaps = JSON.stringify({ query: `
+  query CommonPoaps { Poaps( input: { filter: { owner: { _eq: "${target}" } } blockchain: ALL limit: 100 }) { Poap { poapEvent { poaps(input: { filter: { owner: { _eq: "${requester}" } } }) { eventId mintHash poapEvent { eventName eventURL isVirtualEvent }}}}}}
+  ` });
     //
     // In Phat Contract runtime, we not support async/await, you need use `pink.batchHttpRequest` to
     // send http request. The Phat Contract will return an array of response.
     //
     let responses = pink.batchHttpRequest([
-        { url: apiUrl, method: "POST", headers, body: stringToHex(getUSDTBalance), returnTextBody: true },
-        { url: apiUrl, method: "POST", headers, body: stringToHex(getUSDCBalance), returnTextBody: true },
-        { url: apiUrl, method: "POST", headers, body: stringToHex(getWETHBalance), returnTextBody: true },
+        { url: apiUrl, method: "POST", headers, body: stringToHex(sentTokensToTarget), returnTextBody: true },
         { url: apiUrl, method: "POST", headers, body: stringToHex(hasLensProfile), returnTextBody: true },
         { url: apiUrl, method: "POST", headers, body: stringToHex(hasFarcasterAccount), returnTextBody: true },
         { url: apiUrl, method: "POST", headers, body: stringToHex(hasPrimaryEns), returnTextBody: true },
-        { url: apiUrl, method: "POST", headers, body: stringToHex(hasNFTKinds), returnTextBody: true },
-        { url: apiUrl, method: "POST", headers, body: stringToHex(hasPoapKinds), returnTextBody: true }
+        { url: apiUrl, method: "POST", headers, body: stringToHex(hasCommonPoaps), returnTextBody: true },
     ], 10000 // Param for timeout in milliseconds. Your Phat Contract script has a timeout of 10 seconds
     ); // Notice the [0]. This is important bc the `pink.batchHttpRequest` function expects an array of up to 5 HTTP requests.
     return computeTrustScore(responses);
@@ -151,67 +93,38 @@ function getResponseBody(response) {
     return JSON.parse(response.body);
 }
 function computeTrustScore(responses) {
-    let result = 50; // Start from base score of 60
-    // calculate wallet score
-    let walletScore = 0;
-    const walletWeight = [1, 1, 3000];
-    const usdtBalanceResponseBody = getResponseBody(responses[0]);
-    walletScore += (usdtBalanceResponseBody.data?.ethereum?.TokenBalance[0]?.formattedAmount ?? 0) * walletWeight[0];
-    const usdcBalanceResponseBody = getResponseBody(responses[1]);
-    walletScore += (usdcBalanceResponseBody.data?.ethereum?.TokenBalance[0]?.formattedAmount ?? 0) * walletWeight[1];
-    const wethBalanceResponseBody = getResponseBody(responses[2]);
-    walletScore += (wethBalanceResponseBody.data?.ethereum?.TokenBalance[0]?.formattedAmount ?? 0) * walletWeight[2];
-    if (walletScore < 10000) {
-        result += (30 / Math.log(10001)) * Math.log(walletScore + 1);
-    }
-    else {
-        result += 30;
-    }
-    console.log(`Wallet Score... Result [${result}]`);
-    // calculate social score
-    const socialWeight = [7, 8, 5];
-    const hasLensProfileResponseBody = getResponseBody(responses[3]);
-    result += (hasLensProfileResponseBody.data?.Socials.Social ?? false) ? socialWeight[0] : 0;
+    let result = 0;
+    // Weight values are indexed to map to the index of the responses. e.g. responses[n] => weightValues[n]
+    const weightValues = [10, 7, 7, 10, 7];
+    const sentTokensToTargetResponseBody = getResponseBody(responses[0]);
+    result += (sentTokensToTargetResponseBody.data?.ethereum.TokenTransfer?.length ?? 0) * weightValues[0];
+    console.log(`Tokens Sent on ETH Check... Result [${result}]`);
+    result += (sentTokensToTargetResponseBody.data?.polygon.TokenTransfer?.length ?? 0) * weightValues[0];
+    console.log(`Tokens Sent on ETH Check... Result [${result}]`);
+    const hasLensProfileResponseBody = getResponseBody(responses[1]);
+    result += (hasLensProfileResponseBody.data?.Socials.Social ?? false) ? weightValues[1] : 0;
     console.log(`Lens Profile Check... Result [${result}]`);
-    const hasFarcasterAccountResponseBody = getResponseBody(responses[4]);
-    result += (hasFarcasterAccountResponseBody.data?.Socials.Social ?? false) ? socialWeight[1] : 0;
+    const hasFarcasterAccountResponseBody = getResponseBody(responses[2]);
+    result += (hasFarcasterAccountResponseBody.data?.Socials.Social ?? false) ? weightValues[2] : 0;
     console.log(`Farcaster Account Check... Result [${result}]`);
-    const hasPrimaryEnsResponseBody = getResponseBody(responses[5]);
+    const hasPrimaryEnsResponseBody = getResponseBody(responses[3]);
     const ensDomains = hasPrimaryEnsResponseBody.data?.Domains.Domain ?? [];
     if (ensDomains.length > 0) {
-        if (ensDomains[0].isPrimary) {
-            result += socialWeight[2];
-            console.log(`Primary ENS Account. Result [${result}]`);
+        for (const ensDomain of ensDomains) {
+            if (ensDomain.isPrimary) {
+                result += weightValues[3];
+                console.log(`Primary ENS Account. Result [${result}]`);
+            }
         }
     }
-    // calculate nft score
-    const nftWeight = [30];
-    const hasNFTKindsResponseBody = getResponseBody(responses[6]);
-    const nftKinds = hasNFTKindsResponseBody.data?.ethereum?.TokenBalance?.length ?? 0;
-    if (nftKinds < 10) {
-        result += 3 * nftKinds;
+    const hasCommonPoapsResponseBody = getResponseBody(responses[4]);
+    const commonPoaps = hasCommonPoapsResponseBody.data?.Poaps.Poap ?? [];
+    for (const commonPoap of commonPoaps) {
+        if (commonPoap?.poaps != null) {
+            result += weightValues[4];
+        }
     }
-    else {
-        result += nftWeight[0];
-    }
-    console.log(`NFT Check... Result [${result}]`);
-    // calculate poap score
-    const poapWeight = [30];
-    const hasPoapKindsResponseBody = getResponseBody(responses[7]);
-    const poapKinds = hasPoapKindsResponseBody.data?.Wallet?.poaps?.length ?? 0;
-    if (poapKinds < 10) {
-        result += 3 * poapKinds;
-    }
-    else {
-        result += poapWeight[0];
-    }
-    console.log(`POAP Check... Result [${result}]`);
-    // Apply scaling if result exceeds 90
-    if (result > 90) {
-        let excess = result - 90; // Calculate excess over 90
-        result = 90 + (10 * (Math.log(excess + 1) / Math.log(11))); // Scale excess to 10 points max
-        result = Math.round(result);
-    }
+    console.log(`Common POAPs Check... Result[${result}]`);
     return result;
 }
 //
